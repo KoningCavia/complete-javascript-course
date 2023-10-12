f684fcff33bd422bbcab05759f332a6025f9b2ed
Wicher <wicherICT@hotmail.com>
2023-10-12 18:16:45 +0200

    Section 5: chapter 53 - 58

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,12 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
 
+// in console do live-server to open lie server
+('use strict');
+
+const x = '23';
+
+if (x === 23) console.log(23);
+
+const calcAge = birthyear => 2037 - birthyear;
+
+console.log(calcAge(1991));
