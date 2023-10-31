c985e4e03862905785d368822b6cb9a33ee07524
Wicher <wicherICT@hotmail.com>
2023-10-31 16:13:40 +0100

    js scoping in practive 93

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,29 @@
-'use strict';
+// 'use strict';
+
+function calAge(birthYear) {
+  const age = 2037 - birthYear;
+  console.log(firstName);
+
+  function printAge() {
+    const firstName = 'billy';
+    const output = `${firstName} is ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      const str = `Oh and you are a millenial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+    }
+    console.log(millenial);
+    console.log(add(2, 3));
+  }
+  printAge();
+  return age;
+}
+
+const firstName = 'Jonas';
+calAge(1991);
